678361ec9414e2b5e35164c5b0cbc817829bb32d
K1ra-Python <makismka.guchenko@mail.ru>
2024-03-26 22:11:03 +0300

    #

--- vendor/auto_script.php
+++ vendor/auto_script.php
@@ -6,13 +6,15 @@ require_once 'connect.php';
 $login = $_POST['login'];
 $pass = $_POST['pass'];
 
-$sql = $pdo->prepare("SELECT `LOGIN`, `PASSWORD` FROM `User` WHERE LOGIN = ? AND PASSWORD = ?");
+$sql = $pdo->prepare("SELECT LOGIN, PASSWORD FROM Users WHERE LOGIN = ? AND PASSWORD = ?");
 $sql->execute([$login,$pass]);
 
 $user = $sql->fetch(PDO::FETCH_ASSOC);
 if($user && password_verify($pass,$user['PASSWORD'])){
+    $_SESSION['ERROR_MESSAGE'] = 'ura';
     header('Location: ../admin.php');
 }else{
-    header('auto.php');
+    $_SESSION['ERROR_MESSAGE'] = 'Такой логин уже существует';
+    header('Location: ../auto.php');
     exit();
 }
